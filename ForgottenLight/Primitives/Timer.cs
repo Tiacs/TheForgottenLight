5592496092b7a61f76b03a084ebba123c416fa65
Tiacs <tiacs@tiacs.net>
2019-05-03 23:51:12 +0200

    Implemented Timer class.

---
+++ ForgottenLight/Primitives/Timer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForgottenLight.Primitives {
+    class Timer {
+
+        private int current;
+        private int time;
+        
+        public bool Done {
+            get => current >= time;
+        }
+
+        public Timer(int time) {
+            this.time = time;
+        }
+
+        public void Update(GameTime time) {
+            if(!Done) {
+                current += time.ElapsedGameTime.Milliseconds;
+            }
+        }
+        
+        public void Restart() {
+            this.current = 0;
+        }
+
+    }
+}
