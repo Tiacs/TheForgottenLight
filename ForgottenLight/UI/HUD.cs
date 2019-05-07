345ac5b956a0830550600e3d82c0ce868e3c5d7f
Tiacs <tiacs@tiacs.net>
2019-05-07 10:25:45 +0200

    Refectoring of HUD class.

--- ForgottenLight/UI/HUD.cs
+++ ForgottenLight/UI/HUD.cs
@@ -1,14 +1,10 @@
-﻿using ForgottenLight.Entities;
-using ForgottenLight.Primitives;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using ForgottenLight.Entities;
+using ForgottenLight.Primitives;
 
 namespace ForgottenLight.UI {
     class HUD : UserInterface {
@@ -37,19 +33,14 @@ namespace ForgottenLight.UI {
             };
 
             this.dialogBox = new DialogBox(MainFont, content) {
-                Position = new Vector2(Width / 2, Height - 100),
+                Position = new Vector2(Width / 2, Height - 50),
                 Pivot = new Vector2(.5f, 1),
                 Width = 400,
                 Height = 100,
                 Visible = false,
                 Parent = this
             };
-
-            //this.dialogBox.Messages.Enqueue(new DialogMessage("Hello World!", true));
-            //this.dialogBox.Messages.Enqueue(new DialogMessage("My name is mystic guy!"));
-            //this.dialogBox.Messages.Enqueue(new DialogMessage("I love u!"));
-            //this.dialogBox.Enqueue("This text hopefully is longer then 300px! I hope it will get into the next line! Or maybe antoher one!", false);
-
+            
         }
 
         public override void OnDraw(SpriteBatch spriteBatch, GameTime gameTime) {
